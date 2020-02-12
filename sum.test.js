c20dfcc5c683de7b705c9016b16e0d81ff84d70d
Ryan Doner <ryan.doner@techtonic.com>
2020-02-12 15:30:55 -0700

    modify test

--- sum.test.js
+++ sum.test.js
@@ -2,6 +2,6 @@ import { test, expect } from "jest";
 
 const { notStaff } = require("./src/screens/Helper/helper");
 
-test("adds 1 + 2 to equal 3", () => {
+test("adds 1 + 2to equal 3", () => {
   expect(notStaff(1, 2)).toBe(3);
 });
